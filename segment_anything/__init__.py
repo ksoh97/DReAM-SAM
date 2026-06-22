b772a89df65e4a3aa919e2beed4e8c004f9fb5a7
Kwanseok Oh <ksohh@korea.ac.kr>
2026-06-22 15:31:41 +0900

    Update __init__.py

--- segment_anything/__init__.py
+++ segment_anything/__init__.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+
 from .build_sam import (
     build_sam,
     build_sam_vit_h,
